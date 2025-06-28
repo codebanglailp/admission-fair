1a0ba68fa8413402effeb0a3b7fe213aad850d6c
codebanglailp <code.bangla.ilp@gmail.com>
2025-06-28 18:32:59 +0600

    Initial commit

---
+++ public/src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAuchIAO42bId8fVBEZMN-j_rNsrISkNIs",
+  authDomain: "admission-fair-252.firebaseapp.com",
+  projectId: "admission-fair-252",
+  storageBucket: "admission-fair-252.appspot.com",
+  messagingSenderId: "735315329959",
+  appId: "1:735315329959:web:e7a014693e6da5332e2c82",
+  measurementId: "G-YVTM2HFYLM"
+};
